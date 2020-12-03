8b6fd0baf6a9a0f5c6a86a324d9dd3d4bb344895
irahatam <matahari@nyu.edu>
2020-12-03 00:12:42 -0500

    add all components and router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, BrowserRouter as Router, Redirect } from "react-router-dom";
+import "./App.css";
+
+// Pages
+import Login from "./containers/Login.js";
+import CreateAccount from "./containers/CreateAccount.js";
+import UserProfile from "./containers/UserProfile.js";
+
+// Components
+import Header from "./components/Header.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Router>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+
+        <Route exact path="/create-account">
+          <CreateAccount />
+        </Route>
+
+        <Route exact path="/">
+          <UserProfile />
+        </Route>
+      </Router>
     </div>
   );
 }
